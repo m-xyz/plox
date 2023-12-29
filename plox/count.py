c964c342f056fdc63ba91bf312b4f7100aec238b
miguelamgoncalves <miguel.am.goncalves@ctw.bmwgroup.com>
2023-12-29 21:20:06 +0000

    Lox: prompt

---
+++ plox/count.py
@@ -0,0 +1,7 @@
+x = True
+i = 0
+while x:
+    print(2 ** i)
+    i += 1
+    if(i == 100): x = False
+
--- plox/plox.py
+++ plox/plox.py
@@ -1,39 +1,50 @@
 #! /usr/bin/env python3
 import sys
+import tokenize
 
-def run(src: str):
-    # Tokenize
-    """
-     with tokenize.open('hello.txt') as f:
-        tokens = tokenize.generate_tokens(f.readline)
-        for token in tokens:
-            print(token)
-    """
-    print(src)
+class Plox:
+    broke = False
 
-def run_file(path: str):
-    try:
-        with open(path, 'rb') as file:
-            bytes_data = file.read()
-        run(bytes_data.decode())
-    except Exception as e:
-        print(f"{e}\n")
+    def error(self, line: int, msg: str): report(line, "", msg)
 
-def run_prompt():
-    try:
-        while 1:
-            stream_reader = input("> ")
-            if(stream_reader == None): break
-            run(stream_reader)
-    except KeyboardInterrupt:
-        print(f"\nKeyboardInterrupt\n")
+    def report(self, line: int, where: str, msg: str):
+        print(f"[LINE {line}] Error {where}: {msg}")
+        self.broke = True
+
+    def run(self, src: str):
+        # Placeholder
+        for t in src: print(t)
+
+    def run_file(self, path: str):
+        try:
+            with tokenize.open(path) as file:
+    #            bytes_data = file.read()
+                tkns = tokenize.generate_tokens(file.readline)
+                self.run(tkns)
+            file.close()
+            if(self.broke): exit(65)
+    #        run(bytes_data.decode())
+        except Exception as e:
+            print(f"{e}\n")
+
+    def run_prompt(self):
+        try:
+            while 1:
+                stream_reader = input("> ")
+                if(stream_reader == None): break
+                self.run(stream_reader)
+                self.broke = False
+        except KeyboardInterrupt:
+            print(f"\nKeyboardInterrupt\n")
+
+plox = Plox()
         
 if(len(sys.argv) > 2):
     print("Usage: plox [script]\n")
     exit(64)
-elif(len(sys.argv) == 2): run_file(sys.argv[1])
+elif(len(sys.argv) == 2): plox.run_file(sys.argv[1])
 else:
-    try: run_prompt()
+    try: plox.run_prompt()
     except EOFError: print("\nBye!\n")
 
 
